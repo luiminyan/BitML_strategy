dfc943184a511b6dabb0fefdbaafb674359a454a
Minh <luiminyan@gmail.com>
2024-07-29 14:57:29 +0200

    corrected AS rules

--- Syntax/Strategy.hs
+++ Syntax/Strategy.hs
@@ -1,23 +1,27 @@
 module Syntax.Strategy where
-import Syntax.Common (Time, ID)
+import Syntax.Common (Time, ID, Pred, Condition)
 import Syntax.Label (Label)
 import Syntax.Run (Run(..))
+import NewSet (NewSet)
 
 data Operator = Operator    -- A place holder for operator
+
 
 
 data AbstractStrategy = 
     Do Label 
     | DoNothing
-    | Wait Time
+    | WaitUntil Time
     | Combination Operator AbstractStrategy AbstractStrategy
-    | IfNextThenElse Label [ID] AbstractStrategy AbstractStrategy   
-    | BeforeAfter Time AbstractStrategy AbstractStrategy  
+    | ExecutedThenElse Label AbstractStrategy AbstractStrategy  
+    | IfThenElse Condition AbstractStrategy AbstractStrategy        -- Condition: check Time | Pred
+    
 
 
+-- Result of a strategy on a given run
 data StrategyResult = 
-    Action [Label]
-    | SWait Time
+    Action (NewSet Label)       -- | Action NewSet [Label]
+    | Delay Time
 
 
 -- CS: functions (Run -> StrategyResult)
@@ -25,5 +29,9 @@ type ConcreteStrategy = Run -> StrategyResult
 
 
 
--- evaluation : see AbstractExpr -> ConcreteExpr in FP course
+-- -- evaluation : see AbstractExpr -> ConcreteExpr in FP course
 -- eval :: AbstractStrategy -> ConcreteStrategy
+-- eval IfThenElse p as1 as2 =
+--     case p of
+--         | 
+        
