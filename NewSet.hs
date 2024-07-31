c8c4aca6a83e865e9149ba5ebf2b9feb3959b89f
Minh <luiminyan@gmail.com>
2024-07-31 10:51:30 +0200

    added more functions for NewSet

--- NewSet.hs
+++ NewSet.hs
@@ -4,6 +4,8 @@ Description : A customized set, which includes no duplicate
 -}
 
 module NewSet where
+import Syntax.Label (Label(LWithDraw, LSplit, LPutReveal))
+import Syntax.Common (Participant(Participant), ID (ID), Pred (PTrue))
 
 data NewSet a = EmptySet | UnordSet [a] deriving (Show)
 
@@ -14,20 +16,55 @@ elemSet EmptySet a = False
 elemSet (UnordSet as) a = a `elem` as
 
 
--- insert a new element, exclude duplication
-insert EmptySet a = UnordSet [a]
-insert (UnordSet as) a
+-- insert a new element into a set
+insertElem :: Eq a => NewSet a -> a -> NewSet a
+insertElem EmptySet a = UnordSet [a]
+insertElem (UnordSet as) a
     | elemSet (UnordSet as) a = UnordSet as     -- if a in set, do not insert 
     | otherwise = UnordSet (as ++ [a])             -- insert elem 
 
 
--- union function
-union EmptySet s2 = s2
-union s1 EmptySet = s1
-union s1 (UnordSet as2) = foldl insert s1 as2   -- insert elem from s2 to s1
+-- insert a list into a set
+insertList :: (Eq a) => NewSet a -> [a] -> NewSet a
+insertList= foldl insertElem 
 
 
--- main = do
---     let s1 = insert (UnordSet [1]) 2                         -- UnordSet [1, 2]
---     let s2 = insert (insert EmptySet "Apple") "Banana"       -- UnordSet ["Apple", "Banana"]
---     print s1
+-- union function (all elements from both sets)
+unionSet :: Eq a => NewSet a -> NewSet a -> NewSet a
+unionSet EmptySet s2 = s2
+unionSet s1 EmptySet = s1
+unionSet s1 (UnordSet as2) = foldl insertElem s1 as2   -- insert elem from new set to s1
+
+
+-- negation: s1 - s2
+negationSet EmptySet _ = EmptySet
+negationSet s1 EmptySet = s1
+negationSet (UnordSet s1) s2 = foldl (\acc x -> if elemSet s2 x then acc else insertElem acc x) EmptySet s1 
+
+
+-- map function for NewSet
+mapSet :: (Eq a, Eq b) => (a -> b) -> NewSet a -> NewSet b
+mapSet _ EmptySet = EmptySet
+mapSet f (UnordSet s) = insertList EmptySet (map f s)
+
+
+-- map NewSet into list
+mapSetList :: (a -> b) -> NewSet a -> [b]
+mapSetList _ EmptySet = []
+mapSetList f (UnordSet l) = map f l
+
+
+-- filter function on NewSet
+filterSet :: Eq a => (a -> Bool) -> NewSet a -> NewSet a
+filterSet _ EmptySet = EmptySet
+filterSet f (UnordSet s) = foldl (\acc x -> if f x then insertElem acc x else acc) EmptySet s  
+
+
+main = do
+    let s1 = insertElem (UnordSet [1]) 2                         -- UnordSet [1, 2]
+    let s2 = insertElem (insertElem EmptySet "Apple") "Banana"       -- UnordSet ["Apple", "Banana"]
+    let s3 = insertList EmptySet [2, 3, 1, 4]       
+    let s4 = filterSet even s3      -- UnordSet [2, 4]
+    
+    print $ unionSet s1 s3 
+    print s4        
