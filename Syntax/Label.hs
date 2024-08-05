47128f10aabebc534c8218cd84ddfd8d73fe4a92
Minh <luiminyan@gmail.com>
2024-08-05 13:46:25 +0200

    label: fixed misspelling, withdraw

--- Syntax/Label.hs
+++ Syntax/Label.hs
@@ -1,4 +1,6 @@
-module Syntax.Label where
+module Syntax.Label (
+    Label (..)
+) where
 
 import Syntax.Common ( Secret, Money, Participant, ID, Pred )
 import Syntax.Contract (GuardedContract)
@@ -7,6 +9,6 @@ data Label =
     LSplit ID GuardedContract                               -- avoid ambigious names fors successors -> in the case of choice: split(x) + split(x)
     | LAuthReveal Participant Secret
     | LPutReveal [ID] [Secret] Pred ID GuardedContract      -- avoid ambigious names fors successors -> in the case of choice: put(x) + put(x)   
-    | LWithDraw Participant Money ID
+    | LWithdraw Participant Money ID
     | LAuthControl Participant ID GuardedContract 
     deriving (Eq, Show)
