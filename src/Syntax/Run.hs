f83ffba0afe1e43c6fbe2e5aae34601165b3f6ed
Minh <luiminyan@gmail.com>
2024-08-13 17:33:37 +0200

    corrected transformRun

--- src/Syntax/Run.hs
+++ src/Syntax/Run.hs
@@ -8,14 +8,14 @@ module Syntax.Run (
     , Configuration
     , InitTime (..)
     , Run (..)
-    , removeFirstList
     , removeLastList
     , transformRun
+    , searchTransRun
 ) where
 import Syntax.Label (Label)
 import Syntax.Common ( Secret, Money, Participant, ConcID, Time(..) )
 import Syntax.Contract (Contract, GuardedContract)
-import qualified Data.Map as Map
+import Data.List (find)
 
 
 data ConfigObject =
@@ -36,10 +36,6 @@ data InitTime = InitTime deriving (Show)        -- initTime = Time 0
 newtype Run = Run ((Configuration, InitTime), [(Label ConcID, Configuration, Time)]) deriving (Show)    -- Run (InitConfig, [(label, ConfigList, time)])     
 
 
-removeFirstList :: [a] -> [a]
-removeFirstList [] = []
-removeFirstList (_ : xs) = xs
-
 
 removeLastList :: [a] -> [a]
 removeLastList []       = []
@@ -48,15 +44,25 @@ removeLastList (x : xs) = removeLastList xs ++ [x]
 
 
 
-{- transform run into {label1: ((initConf, t0), (conf1, t1)), ... labelj: ((confi, ti), (congj, tj))...}
+{- transform run into a list [(label1, ((initConf, t0), (conf1, t1))), ... (labelj, ((confi, ti), (congj, tj)))...]
+    init-run (no label executed), return []
 -}
-transformRun :: Run -> Map.Map (Label ConcID) ((Configuration, Time), (Configuration, Time))
+transformRun :: Run -> [(Label ConcID, ((Configuration, Time), (Configuration, Time)))]
+transformRun (Run (_, [])) = []
 transformRun (Run ((initConf, _), tplList@((label1, confList1, time1) : xsi)))  =
     case length tplList of
-        0   -> Map.empty
-        1   -> fstPairDict
+        1   -> fstPairList
         _   ->
-            let configsList = zip [(c, t) | (l, c, t) <- removeFirstList xsi] [(c, t) | (l, c, t) <- removeLastList xsi] in     -- [((bf_conf, t), (aft_conf, t'))] 
-                let labelConfigsList = zip [l | (l, _, _) <- removeFirstList xsi] configsList in                                -- [(label, ((confi, t1), (confj, tj)))]
-                    foldl (\acc (label, confTupel) -> Map.insert label confTupel acc) fstPairDict labelConfigsList
-    where fstPairDict = Map.insert label1 ((initConf, Time 0), (confList1, time1)) Map.empty        -- {label1: ((initConf, Time 0), (confList1, time1))}
+            let configsList = zip [(c, t) | (_, c, t) <- removeLastList tplList] [(c, t) | (_, c, t) <- xsi] in     -- [((bf_conf, t), (aft_conf, t'))] 
+                let labelConfigsList = zip [l | (l, _, _) <- xsi] configsList in                                -- [(label, ((confi, t1), (confj, tj)))]
+                    foldl (\acc labelConfTupel -> acc ++ [labelConfTupel]) fstPairList labelConfigsList
+    where fstPairList = [(label1, ((initConf, Time 0), (confList1, time1)))]        -- {label1: ((initConf, Time 0), (confList1, time1))}
+
+
+
+{- search for a label in the transformed-run
+    exist: return the first found item ((ConfBF, t), (ConfAFT, t'))
+    else: Nothing
+-}
+searchTransRun :: Label ConcID -> [(Label ConcID, ((Configuration, Time), (Configuration, Time)))] -> Maybe ((Configuration, Time), (Configuration, Time))
+searchTransRun label transRun = fmap snd (find (\(l, _) -> label == l) transRun)   
