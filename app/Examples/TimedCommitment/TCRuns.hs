abecd687fa17cbd51a3c18ac44dc808ea1f712a8
Minh <luiminyan@gmail.com>
2024-08-15 14:18:15 +0200

    updated TCRun with appendRun

--- app/Examples/TimedCommitment/TCRuns.hs
+++ app/Examples/TimedCommitment/TCRuns.hs
@@ -17,31 +17,12 @@ run_tc_0 :: Run
 run_tc_0 = Run ([ActiveContract contractTimedCommitment (BCoins 1) (ConcID "tc")], [])
 
 runTCReva :: Run
-runTCReva = Run (
-                    [ActiveContract contractTimedCommitment (BCoins 1) (ConcID "tc")]
-                    , [
-                        (LAuthReveal (Participant "A") (Secret "a")
-                        , [ActiveContract [PutReveal [] [Secret "a"] PTrue [Withdraw (Participant "A")]] (BCoins 1) (ConcID "tc")
-                            , RevealedSecret (Participant "A") (Secret "a") 10]
-                        , Time 0)
-                    ]
-                )
+runTCReva = appendRun (LAuthReveal (Participant "A") (Secret "a")) [ActiveContract [PutReveal [] [Secret "a"] PTrue [Withdraw (Participant "A")]] (BCoins 1) (ConcID "tc"), RevealedSecret (Participant "A") (Secret "a") 10] (Time 0) run_tc_0
+
 
 runTCPuta :: Run
-runTCPuta = Run (
-                    [ActiveContract contractTimedCommitment (BCoins 1) (ConcID "tc")]
-                    , [
-                        (LAuthReveal (Participant "A") (Secret "a")
-                        , [ActiveContract contractTimedCommitment (BCoins 1) (ConcID "tc")
-                            , RevealedSecret (Participant "A") (Secret "a") 10]
-                        , Time 0)
-                        , (LPutReveal [] [Secret "a"] PTrue (ConcID "tc") [Withdraw (Participant "A")]
-                            , [ActiveContract [Withdraw (Participant "A")] (BCoins 1) (ConcID "x")
-                                , RevealedSecret (Participant "A") (Secret "a") 10]
-                            , Time 0
-                        )
-                    ]
-                )
+runTCPuta = appendRun (LPutReveal [] [Secret "a"] PTrue (ConcID "tc") [Withdraw (Participant "A")]) [ActiveContract [Withdraw (Participant "A")] (BCoins 1) (ConcID "x"), RevealedSecret (Participant "A") (Secret "a") 10] (Time 0) runTCReva
+
 
 runTCWithdrawB :: Run
 runTCWithdrawB = Run ([ActiveContract contractTimedCommitment (BCoins 1) (ConcID "tc")], [(LDelay (Time 2), [ActiveContract contractTimedCommitment (BCoins 1) (ConcID "tc")], Time 2)])
