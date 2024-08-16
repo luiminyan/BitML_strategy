da523cbadc1ab8481eb5cbd7912e88f78d936de4
Minh <luiminyan@gmail.com>
2024-08-16 15:42:43 +0200

    corrected startegy

--- app/Examples/RockPaperScissors/RPSStrategies.hs
+++ app/Examples/RockPaperScissors/RPSStrategies.hs
@@ -1,5 +1,8 @@
 module Examples.RockPaperScissors.RPSStrategies (
-    paRpsStrategy
+    paRPSStrategy
+    , paRpsStrategy0
+    , paRpsStrategy1
+    , paRpsStrategy2
 ) where
 import Syntax.Strategy
 import Examples.RockPaperScissors.RPSContract
@@ -8,8 +11,8 @@ import Syntax.Common
 
 
 
-paRpsStrategy :: AbstractStrategy
-paRpsStrategy = ExecutedThenElse (LSplit (CID (ConcID "rps")) [(BCoins 2, rpsCon0), (BCoins 2, rpsCon1), (BCoins 2, rpsCon2)]) [VarID "x0", VarID "x1", VarID "x2"]
+paRPSStrategy :: AbstractStrategy
+paRPSStrategy = ExecutedThenElse (LSplit (CID (ConcID "rps")) [(BCoins 2, rpsCon0), (BCoins 2, rpsCon1), (BCoins 2, rpsCon2)]) [VarID "x0", VarID "x1", VarID "x2"]
     (Combination (Combination paRpsStrategy0 paRpsStrategy1) paRpsStrategy2)
     (Do (LSplit (CID (ConcID "rps")) [(BCoins 2, rpsCon0), (BCoins 2, rpsCon1), (BCoins 2, rpsCon2)]))
 
@@ -24,45 +27,46 @@ paRpsStrategy0 = IfThenElse (BeforeTimeOut timeD)
 
 
 paRpsStrategy1 :: AbstractStrategy
-paRpsStrategy1 = IfThenElse (BeforeTimeOut timeD)
-    (ExecutedThenElse (LAuthReveal pa secA) []
-        (IfThenElse (Predicate (PBtwn (EInt 0) (ELength secA) (EInt 2)))
-            (ExecutedThenElse (LPutReveal [] [secA] (PBtwn (EInt 0) (ELength secA) (EInt 2)) (VID (VarID "x1")) [gConWithdrawPA]) [VarID "x1*"]
-                (ExecutedThenElse (LWithdraw pa (BCoins 2) (VID (VarID "x1*"))) []
-                    DoNothing
-                    (Do (LWithdraw pa (BCoins 2) (VID (VarID "x1*"))))
+paRpsStrategy1 = 
+    IfThenElse (BeforeTimeOut timeD)
+        (ExecutedThenElse (LAuthReveal pa secA) []
+            (IfThenElse (Predicate (PBtwn (EInt 0) (ELength secA) (EInt 2)))
+                (ExecutedThenElse (LPutReveal [] [secA] (PBtwn (EInt 0) (ELength secA) (EInt 2)) (VID (VarID "x1")) [gConWithdrawPA]) [VarID "x1*"]
+                    (ExecutedThenElse (LWithdraw pa (BCoins 2) (VID (VarID "x1*"))) []
+                        DoNothing
+                        (Do (LWithdraw pa (BCoins 2) (VID (VarID "x1*"))))
+                    )
+                    (Do (LPutReveal [] [secA] (PBtwn (EInt 0) (ELength secA) (EInt 2)) (VID (VarID "x1")) [gConWithdrawPA]))
                 )
-                (Do (LPutReveal [] [secA] (PBtwn (EInt 0) (ELength secA) (EInt 2)) (VID (VarID "x1")) [gConWithdrawPA]))
+                DoNothing
             )
-            DoNothing
+            (Do (LAuthReveal pa secA))
         )
-        (Do (LAuthReveal pa secA))
-    )
-    DoNothing
+        DoNothing
 
 
 paRpsStrategy2 :: AbstractStrategy
 paRpsStrategy2 =
     ExecutedThenElse (LAuthReveal pa secA) []
         (ExecutedThenElse (LAuthReveal pb secB) []
-            (IfThenElse (Predicate (PLt (ELength secB) (ELength secA)))
-                (ExecutedThenElse (LPutReveal [] [secA, secB] (win secA secB) (VID (VarID "x3")) [gConWithdrawPA]) [VarID "x3*"]
-                    (ExecutedThenElse (LWithdraw pa (BCoins 2) (VID (VarID "x3*"))) []
+            (IfThenElse (Predicate (win secA secB))
+                (ExecutedThenElse (LPutReveal [] [secA, secB] (win secA secB) (VID (VarID "x2")) [gConWithdrawPA]) [VarID "x2*"]
+                    (ExecutedThenElse (LWithdraw pa (BCoins 2) (VID (VarID "x2*"))) []
                         DoNothing
-                        (Do (LWithdraw pa (BCoins 2) (VID (VarID "x3*"))))
+                        (Do (LWithdraw pa (BCoins 2) (VID (VarID "x2*"))))
                     )
-                    (Do (LPutReveal [] [secA, secB] (win secA secB) (VID (VarID "x3")) [gConWithdrawPA]))
+                    (Do (LPutReveal [] [secA, secB] (win secA secB) (VID (VarID "x2")) [gConWithdrawPA]))
                 )
                 (IfThenElse (Predicate (PEq (ELength secA) (ELength secB)))
-                    (ExecutedThenElse (LPutReveal [] [secA, secB] (PEq (ELength secA) (ELength secB)) (VID (VarID "x3")) rpsCon2Split) [VarID "x3**"]
-                        (ExecutedThenElse (LSplit (VID (VarID "x3**")) [(BCoins 1, [gConWithdrawPA]), (BCoins 1, [gConWithdrawPB])]) [VarID "x4", VarID "x5"]
-                            (ExecutedThenElse (LWithdraw pa (BCoins 1) (VID (VarID "x4"))) []
+                    (ExecutedThenElse (LPutReveal [] [secA, secB] (PEq (ELength secA) (ELength secB)) (VID (VarID "x2")) rpsCon2Split) [VarID "x2*"]
+                        (ExecutedThenElse (LSplit (VID (VarID "x2*")) [(BCoins 1, [gConWithdrawPA]), (BCoins 1, [gConWithdrawPB])]) [VarID "x3", VarID "x4"]
+                            (ExecutedThenElse (LWithdraw pa (BCoins 1) (VID (VarID "x3"))) []
                                 DoNothing
-                                (Do (LWithdraw pa (BCoins 1) (VID (VarID "x4"))))
+                                (Do (LWithdraw pa (BCoins 1) (VID (VarID "x3"))))
                             )
-                            (Do (LSplit (VID (VarID "x3**")) [(BCoins 1, [gConWithdrawPA]), (BCoins 1, [gConWithdrawPB])]))
+                            (Do (LSplit (VID (VarID "x2*")) [(BCoins 1, [gConWithdrawPA]), (BCoins 1, [gConWithdrawPB])]))
                         )
-                        (Do (LPutReveal [] [secA, secB] (PEq (ELength secA) (ELength secB)) (VID (VarID "x3")) rpsCon2Split))
+                        (Do (LPutReveal [] [secA, secB] (PEq (ELength secA) (ELength secB)) (VID (VarID "x2")) rpsCon2Split))
                     )
                     DoNothing
                 )
