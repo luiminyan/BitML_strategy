ca614845c6e5e3ed51180d6186eb54c96fa9d418
Minh <luiminyan@gmail.com>
2024-08-21 13:21:48 +0200

    added resolveID test

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,8 @@ import Test.HUnit
 import PredicateTests
 import NewSetTest
 import OperatorTest
+import RunTest
 
 main :: IO Counts
--- main = runTestTT $ TestList [predicateTests, newSetTest]
-main = runTestTT $ TestList [resolveCIDTest]
+main = do 
+    runTestTT $ TestList [predicateTests, newSetTest, resolveCIDTest, runTestList]
